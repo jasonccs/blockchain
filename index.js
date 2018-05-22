32ac342164fe4a32c8a2cda3b1eabe3550a97d03
jasonccs <1521910992@qq.com>
2018-05-22 09:47:06 +0800

    '区块链'

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
 VotingContract = web3.eth.contract(abi);
 console.info("==="+VotingContract.address);
-contractInstance = VotingContract.at('0xc1bb149d9e8a12470f00a7fe6a9cdf950d9e18c8');
-candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
+contractInstance = VotingContract.at('0xb05768248a1f3e87f361803d7755ea0f8cca8851');
+candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"};
 
 function voteForCandidate(candidate) {
  candidateName = $("#candidate").val();
@@ -20,7 +20,7 @@ $(document).ready(function() {
  candidateNames = Object.keys(candidates);
  for (var i = 0; i < candidateNames.length; i++) {
   let name = candidateNames[i];
-  let val = contractInstance.totalVotesFor.call(name).toString()
+  let val = contractInstance.totalVotesFor.call(name).toString();
   $("#"+candidates[name]).html(val);
  }
 });
